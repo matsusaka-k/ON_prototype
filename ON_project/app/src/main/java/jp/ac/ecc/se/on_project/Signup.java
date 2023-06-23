c8191e83be0e00c69f4c48d3aa247b528a54f0e6
平田愛奈 <2220224@ecc.ac.jp>
2023-06-23 10:42:58 +0900

    login signup

--- ON_project/app/src/main/java/jp/ac/ecc/se/on_project/Signup.java
+++ ON_project/app/src/main/java/jp/ac/ecc/se/on_project/Signup.java
@@ -3,6 +3,10 @@ package jp.ac.ecc.se.on_project;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class Signup extends AppCompatActivity {
 
@@ -10,5 +14,48 @@ public class Signup extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
+        // 画面パーツ情報登録
+        // 文字
+        TextView login = findViewById(R.id.Text);
+        // USERID
+        EditText UserID = findViewById(R.id.UserId);
+        // パスワード
+        EditText Password = findViewById(R.id.Password);
+        // アカウント作成ボタン
+        Button CreationButton = findViewById(R.id.CreationButton);
+
+
+        // 登録ユーザーID　８文字以上　かぶってたらトーストでだす
+
+
+
+
+
+        // パスワード　８文字以上１４文字の半角英数字　
+
+
+
+
+
+        // データベース接続？
+
+
+
+
+
+
+
+        // アカウント作成したらログイン画面に戻る　遷移
+        CreationButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+
+
+
     }
 }
--- ON_project/app/src/main/java/jp/ac/ecc/se/on_project/login.java
+++ ON_project/app/src/main/java/jp/ac/ecc/se/on_project/login.java
@@ -2,13 +2,67 @@ package jp.ac.ecc.se.on_project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class login extends AppCompatActivity {
 
+
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+    protected void onCreate(Bundle savedInstanceState){
+     super.onCreate(savedInstanceState);
+     setContentView(R.layout.activity_login);
+
+
+         // 画面パーツ情報
+         // 文字
+         TextView login = findViewById(R.id.Text);
+         // USERID
+         EditText UserID = findViewById(R.id.UserId);
+         // パスワード
+         EditText Password = findViewById(R.id.Password);
+         // ログイン画面
+         Button LoginButton = findViewById(R.id.LoginButton);
+         // 新規登録ボタン
+         Button SignupButton = findViewById(R.id.SignupButton);
+
+
+
+         // 新規登録ボタンが押されたらSignup画面に遷移
+        SignupButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplication(), Signup.class);
+                startActivity(intent);
+            }
+        });
+
+
+        // ログインボタンが押されたらマイルーム画面に遷移
+        /*
+        LoginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), Myroom.class);
+                startActivity(intent);
+            }
+        });
+
+         */
+
+
+
+
+            // SQL文
+
+
+
+
+
     }
 }
